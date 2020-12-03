32f41431776481f8b2e1f96f2c8b4c61c616d872
robertusbagaskara <robertusbagaskara@gmail.com>
2020-12-03 07:58:51 +0700

    missing number

---
+++ missingnumbers.py
@@ -0,0 +1,18 @@
+looping = int(input())
+a = []
+b = []
+missingNumber = []
+for i in range(looping):
+    number = int(input())
+    a.append(number)
+for i in range(a[looping-1]):
+    b.append(i+1)
+    if(b[i] not in a):
+        missingNumber.append(b[i])
+
+if(missingNumber == []):
+    print('good job')
+else:
+    for i in range(len(missingNumber)):
+        print(missingNumber[i])
+
