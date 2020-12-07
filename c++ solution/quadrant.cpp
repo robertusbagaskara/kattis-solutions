e48132a034e532e8243dd77fb0f7859017ffe381
robertusbagaskara <robertusbagaskara@gmail.com>
2020-12-07 20:16:10 +0700

    add quadrant selection

---
+++ c++ solution/quadrant.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	if (x>=0 && y>=0) {
+		cout << "1";
+	} else if (x>=0 && y<0) {
+		cout << "4";
+	} else if (x<0 && y<0) {
+		cout << "3";
+	} else {
+		cout << "2";
+	}
+}
