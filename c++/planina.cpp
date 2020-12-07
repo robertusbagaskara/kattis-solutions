152a05f408245efcab82409b18ddb7640ef00e69
robertusbagaskara <robertusbagaskara@gmail.com>
2020-12-07 23:34:42 +0700

    add planina

---
+++ c++/planina.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int n, num;
+	cin >> n;
+	num = pow(2, n)+1;
+	cout << num * num ;
+	return 0;
+}
---
+++ python/planina.py
@@ -0,0 +1,2 @@
+iteration = int(input())
+print(((2**iteration)+1)**2)
