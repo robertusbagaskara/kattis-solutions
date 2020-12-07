e5fce22141e39e3ccd18c93bcb1b3a0310408ed0
robertusbagaskara <robertusbagaskara@gmail.com>
2020-12-07 19:38:08 +0700

    add take two stones

---
+++ c++ solution/twostones.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int number;
+	cin >> number;
+	if (number%2==0) {
+		cout << "Bob";
+	} else {
+		cout << "Alice";
+	}
+}
---
+++ python solution/twostones.py
@@ -0,0 +1,5 @@
+num = int(input())
+if(num%2==0):
+    print('Bob')
+else:
+    print('Alice')
