944ab4ed243bd3a378d5e22bc61e126c52bfb076
robertusbagaskara <robertusbagaskara@gmail.com>
2020-12-09 07:57:27 +0700

    100%

--- c++ solution/ofugsnuid.cpp
+++ c++ solution/ofugsnuid.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-	int i, j, n;
-	cin >> n;
-	int *arr = new int(n);
-	for (i=0; i<n; i++) {
-		cin >> arr[i];
-	}
-	for (j=n-1; j>=0; j--) {
-		cout << arr[j] << "\n";
-	}
-	return 0;
+    int i, j, n;
+    cin >> n;
+    int arr[n];
+    for (i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    for (j=n-1; j>=0; j--) {
+        cout << arr[j] << "\n";
+    }
+    return 0;
 }
