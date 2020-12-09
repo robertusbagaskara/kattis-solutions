eb0a0b57a64264ad586491bd6129856ca8a9499c
robertusbagaskara <robertusbagaskara@gmail.com>
2020-12-09 07:18:07 +0700

    only 50% c++

---
+++ c++ solution/ofugsnuid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, j, n;
+	cin >> n;
+	int *arr = new int(n);
+	for (i=0; i<n; i++) {
+		cin >> arr[i];
+	}
+	for (j=n-1; j>=0; j--) {
+		cout << arr[j] << "\n";
+	}
+	return 0;
+}
