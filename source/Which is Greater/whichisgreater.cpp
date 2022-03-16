5bbba6c785eda2379a5d4b474cbfcd139441458f
robertusbagaskara <robertusbagaskara@gmail.com>
2022-03-16 13:35:42 +0700

    add solutions

---
+++ source/Which is Greater/whichisgreater.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long int a, b;
+    cin >> a >> b;
+    if (a > b) {
+        cout << 1 << "\n";
+    } else {
+        cout << 0 << "\n";
+    }
+}
