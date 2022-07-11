293ae464d13c7c588d403d3f76492e7f40ab9345
robertusbagaskara <robertusbagaskara@gmail.com>
2022-07-11 10:52:50 +0700

    update kattis scrapper

--- kattis_scrapper.py
+++ kattis_scrapper.py
@@ -1,19 +1,17 @@
+"""
+Scrapper for getting point of each problem in Kattis Website
+
+v.1.1.1
+"""
+
+from attr import s
 import requests
 from bs4 import BeautifulSoup
 import re 
-# import time 
-
-# start_time = time.time()
+import time 
 
 class PointScrapper: 
     def getPoints(self, url):
         r = requests.get(url)
-        # point = re.search("<span>...</span>|", str(r.content))
-        # return point.group().replace("<span>", "").replace("</span>", "")
         soup = BeautifulSoup(r.content, 'html5lib')
-        return(str(soup.find_all('span')[-1]).replace("<span>", "").replace("</span>", ""))  
-
-# test = PointScrapper()
-# test.getPoints("https://open.kattis.com/problems/logo")
-
-# print("--- Done in %s seconds ---" % (time.time() - start_time))
+        return str(soup.find('span', class_='difficulty_number').get_text())
