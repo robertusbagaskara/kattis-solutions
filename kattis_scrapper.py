41bfc05622feaee1ff241564e2521d800f81c892
robertusbagaskara <robertusbagaskara@gmail.com>
2022-07-29 08:33:30 +0700

    tidying up

--- kattis_scrapper.py
+++ kattis_scrapper.py
@@ -4,11 +4,8 @@ Scrapper for getting point of each problem in Kattis Website
 v.1.1.1
 """
 
-from attr import s
 import requests
 from bs4 import BeautifulSoup
-import re 
-import time 
 
 class PointScrapper: 
     def getPoints(self, url):
