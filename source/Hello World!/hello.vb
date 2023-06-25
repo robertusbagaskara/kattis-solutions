418e2044e83b0a9e1a334e6f7b43cee19f09fa1e
robertusbagaskara <robertusbagaskara@gmail.com>
2023-06-25 19:22:02 +0700

    testing vb

---
+++ source/Hello World!/hello.vb
@@ -0,0 +1,11 @@
+Imports System
+
+option explicit
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
+
+' get 0.03s execution time in Kattis CPU
