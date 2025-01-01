141a089c07cc0f94ec286295959951e54c7f9494
robertusbagaskara <robertusbagaskara@gmail.com>
2025-01-01 23:08:12 +0700

    back to the game

---
+++ source/Takkar/takkar.py
@@ -0,0 +1,9 @@
+trumpButton = int(input())
+kimButton = int(input())
+
+if trumpButton > kimButton:
+    print("MAGA!")
+elif trumpButton < kimButton:
+    print("FAKE NEWS!")
+elif trumpButton == kimButton:
+    print("WORLD WAR 3!") 
