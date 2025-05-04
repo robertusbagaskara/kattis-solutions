f48a14694f2cc85a70909395f1b147aeaea2db15
robertusbagaskara <robertusbagaskara@gmail.com>
2025-05-04 23:16:00 +0700

    fix beautifulsoup

--- kattis_scrapper.py
+++ kattis_scrapper.py
@@ -10,5 +10,6 @@ from bs4 import BeautifulSoup
 class PointScrapper: 
     def getPoints(self, url):
         r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'html5lib')
+        #soup = BeautifulSoup(r.content, 'html5lib')
+        soup = BeautifulSoup(r.content, "lxml")
         return str(soup.find('span', class_='difficulty_number').get_text())
